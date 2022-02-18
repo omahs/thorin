7d39d85e1b2144a52df79c614b939f635bf393d0
Jeff Lau <jeff-lau@live.com>
2022-02-18 20:03:19 +0800

    WIP

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -3,7 +3,9 @@ import { AppProps } from 'next'
 import { MDXProvider } from '@mdx-js/react'
 import Head from 'next/head'
 
-import { ThemeProvider } from '@ensdomains/thorin/components'
+import { ThemeProvider as ThemeProviderDeprecated } from '@ensdomains/thorin/components'
+import { lightTheme } from '@ensdomains/thorin/styles/vars.css'
+import { ThemeProvider } from 'styled-components'
 
 import { MDX } from '~/components'
 import { getLayout as getDocsLayout } from '~/layouts/docs'
@@ -14,22 +16,24 @@ const App = ({ Component, pageProps }: AppProps) => {
   const getLayout = Component.getLayout || getDocsLayout
 
   return (
-    <ThemeProvider
-      defaultAccent={getThemeAccent()}
-      defaultMode={getThemeMode() ?? 'light'}
-    >
-      <Head>
-        {/* Prevent theme flash */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(){try{var d=document.documentElement;var e=document.cookie.split(";").find(x=>x.includes("mode"));if(e){d.setAttribute('data-theme',e.replace("mode=","").trim())}else{d.setAttribute('data-theme','light');}}catch(t){}}();`,
-          }}
-        />
-      </Head>
+    <ThemeProvider theme={lightTheme}>
+      <ThemeProviderDeprecated
+        defaultAccent={getThemeAccent()}
+        defaultMode={getThemeMode() ?? 'light'}
+      >
+        <Head>
+          {/* Prevent theme flash */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(){try{var d=document.documentElement;var e=document.cookie.split(";").find(x=>x.includes("mode"));if(e){d.setAttribute('data-theme',e.replace("mode=","").trim())}else{d.setAttribute('data-theme','light');}}catch(t){}}();`,
+            }}
+          />
+        </Head>
 
-      <MDXProvider components={MDX}>
-        {getLayout(<Component {...pageProps} />)}
-      </MDXProvider>
+        <MDXProvider components={MDX}>
+          {getLayout(<Component {...pageProps} />)}
+        </MDXProvider>
+      </ThemeProviderDeprecated>
     </ThemeProvider>
   )
 }
